e54709d70e950c88525b4d5eb59ed3d8e55eac96
Sarah Heinz. <124076207+SarahHeinz@users.noreply.github.com>
2024-02-25 21:02:31 +0100

    Added Languages

--- js/app.js
+++ js/app.js
@@ -1,9 +1,30 @@
+
+// ------------NAV MENU
+
 function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  } 
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+
+// ----------ACCESSIBILITY BUTTON
+const accessible = document.querySelector (".accessible");
+// const accessibility = document.querySelector(".accessibility");
+const body = document.querySelector("complex");
+
+accessible.addEventListener('click', () => body.classList.add('add-accessibility'));
+
+
+// accessibility.addEventlistener("click", function () {
+//   if (body.classList.contains("addAccessible")) {
+//     body.classList.remove("addAccessible");
+//   } else {
+//     body.classList.add("addAccessible");
+
+//   }
+// })
 
